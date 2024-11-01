8bc060c0f1263b2331b944dc784ef91ddd1fad65
Perry02 <59411123+Perry02@users.noreply.github.com>
2024-11-01 16:06:40 +0100

    setup possible peer actions in interface

---
+++ DAPM/DAPM.PeerApi/Services/Interfaces/IPeerUserService.cs
@@ -0,0 +1,23 @@
+﻿using UtilLibrary.Interfaces;
+using UtilLibrary.models;
+
+namespace DAPM.PeerApi.Services.Interfaces
+{
+    public interface IPeerUserService
+    {
+        // Users
+
+        public void OnGetPeerUsers();
+
+        // invite guest user (term to denote user from other org)
+
+        public void OnInvitePeerUser(Identity identity);
+
+        // Resources
+
+        public void OnGetSharedPeerResources(Identity identity);
+
+        // proxy versions of original, that syncs actions with originating peer.
+
+    }
+}
