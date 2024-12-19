4d0b527e1e09d136c090cc54c9770d7c2b9cc5b1
SuhailDTU <s205135@student.dtu.dk>
2024-12-19 19:23:27 +0100

    conflict

--- Unit_tests/obj/Debug/net8.0/Unit_tests.AssemblyInfo.cs
+++ Unit_tests/obj/Debug/net8.0/Unit_tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unit_tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd05745b13c6b163e708156e74b77a255dd05e0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3646594d44cf1d3363b3925c542e89087b9d8fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unit_tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unit_tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
